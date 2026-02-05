f4e1929ccc8d72ec62170a125edf0702f3a53e5d
Bhargav Kulkarni <bhargav.kulkarni1@gmail.com>
2026-02-04 18:36:34 -0700

    Saving changes

--- LambdaSkia/CoreSk.lean
+++ LambdaSkia/CoreSk.lean
@@ -10,27 +10,24 @@ def Point : Type := Unit
 @[grind, simp]
 def Geometry : Type := Point -> Bool
 
-@[grind, simp]
-def Fill : Type := Point -> Pixel -- make this Pixel | Point -> Pixel
-
 @[grind]
-inductive Fill' : Type where
-  | pixel : Pixel -> Fill'
-  | shader : (Point -> Pixel) -> Fill'
+inductive Fill : Type where
+  | pixel : Pixel -> Fill
+  | shader : (Point -> Pixel) -> Fill
 
 @[grind, simp]
-def getFillFunc (f : Fill') : Point -> Pixel :=
+def getFillFunc (f : Fill) : Point -> Pixel :=
 match f with
-  | Fill'.pixel px => fun _ => px
-  | Fill'.shader f => f
+  | Fill.pixel px => fun _ => px
+  | Fill.shader f => f
 
 @[simp, grind =]
 lemma getFillFunc_pixel (px) (pt) :
-  getFillFunc (Fill'.pixel px) pt = px := rfl
+  getFillFunc (Fill.pixel px) pt = px := rfl
 
 @[simp, grind =]
 lemma getFillFunc_shader (f) (pt) :
-  getFillFunc (Fill'.shader f) pt = f pt := rfl
+  getFillFunc (Fill.shader f) pt = f pt := rfl
 
 @[grind, simp]
 def BlendMode : Type := Pixel -> Pixel -> Pixel
@@ -42,21 +39,21 @@ def Style : Type := Geometry -> Geometry
 def Filter : Type := Pixel -> Pixel
 
 @[grind, simp]
-def Paint : Type := Fill' × BlendMode × Style × Filter
+def Paint : Type := Fill × BlendMode × Style × Filter
 
 @[grind, simp]
-def getAlpha (p : Fill') : Real :=
+def getAlpha (p : Fill) : Real :=
   match p with
-  | Fill'.pixel px => px.1
-  | Fill'.shader _ => 1
+  | Fill.pixel px => px.1
+  | Fill.shader _ => 1
 
 @[simp, grind =]
 lemma getAlpha_pixel (px) :
-  getAlpha (Fill'.pixel px) = px.1 := rfl
+  getAlpha (Fill.pixel px) = px.1 := rfl
 
 @[simp, grind =]
 lemma getAlpha_shader (f) :
-  getAlpha (Fill'.shader f) = 1 := rfl
+  getAlpha (Fill.shader f) = 1 := rfl
 
 @[grind]
 inductive Layer : Type where
@@ -87,8 +84,7 @@ notation "⟦" l "⟧" => denote l
 theorem OpaqueSaveLayerEmptyLayer l_b :
   denote (saveLayer l_b
                     empty
-                    (Fill'.pixel (Alpha 1), srcover, id, id)
-                    )
+                    (Fill.pixel (Alpha 1), srcover, id, id))
   =
   denote l_b :=
 by
@@ -101,8 +97,7 @@ theorem OpaqueSaveLayerRemoveLoneDraw
   (clip : Geometry) :
   denote (saveLayer empty
                     (draw empty g paint clip)
-                    (Fill'.pixel (Alpha 1), srcover, id, id)
-                    )
+                    (Fill.pixel (Alpha 1), srcover, id, id))
   =
   denote (draw empty g paint clip) :=
 by
@@ -112,17 +107,17 @@ by
 theorem OpaqueSaveLayerRemoveLastDraw
   (l₁ l₂ : Layer)
   (g clip : Geometry)
-  (fill : Fill')
+  (fill : Fill)
   (style : Style)
   (filter : Filter) :
   denote
     (saveLayer l₁
       (draw l₂ g (fill, srcover, style, filter) clip)
-      (Fill'.pixel (Alpha 1), srcover, id, id))
+      (Fill.pixel (Alpha 1), srcover, id, id))
   =
   denote
     (draw
-      (saveLayer l₁ l₂ (Fill'.pixel (Alpha 1), srcover, id, id))
+      (saveLayer l₁ l₂ (Fill.pixel (Alpha 1), srcover, id, id))
       g (fill, srcover, style, filter) clip) :=
 by
   ext pt
@@ -137,11 +132,11 @@ theorem SubsumeColorFilter
   (H : f Transparent = Transparent) :
   denote
     (saveLayer empty
-      (draw empty g (Fill'.pixel c, srcover, style, id) clip)
-      (Fill'.pixel (Alpha 1), srcover, id, f))
+      (draw empty g (Fill.pixel c, srcover, style, id) clip)
+      (Fill.pixel (Alpha 1), srcover, id, f))
   =
   denote
-    (draw empty g (Fill'.pixel (f c), srcover, style, id) clip) :=
+    (draw empty g (Fill.pixel (f c), srcover, style, id) clip) :=
 by
   ext pt
   simp
@@ -151,16 +146,16 @@ theorem better_masks
   (shape clip1 clip2 : Geometry)
   (style : Style)
   (color : Pixel)
-  (gradient : Fill')
+  (gradient : Fill)
   (Hsubset : ∀ pt, clip1 pt = true → clip2 pt = true)
   (Hopaque : ∀ pt, ((getFillFunc gradient) pt).a = 1) :
   denote
     (saveLayer
-      (draw empty shape (Fill'.pixel color, srcover, style, id) clip1)
+      (draw empty shape (Fill.pixel color, srcover, style, id) clip1)
       (draw empty shape (gradient, srcover, style, id) clip2)
-      (Fill'.pixel (Alpha 1), dstin, id, id))
+      (Fill.pixel (Alpha 1), dstin, id, id))
   =
-  denote (draw empty shape (Fill'.pixel color, srcover, style, id) clip1) :=
+  denote (draw empty shape (Fill.pixel color, srcover, style, id) clip1) :=
 by
   ext pt
   simp only [denote]
